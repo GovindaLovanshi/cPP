340f945f7a8835a06d1de3a68333e087f794d823
Govinda Lovanshi <134533021+GovindaLovanshi@users.noreply.github.com>
2024-10-15 11:48:00 +0530

    Add files via upload

---
+++ class Secound.java
@@ -0,0 +1,10 @@
+class Secound
+{
+  public static void main (String ar[])
+{
+    int a=70;
+     intb=90;
+   System .out.println("value of a=",+a);
+   System .out.println("value of b=",+b);
+}
+}
